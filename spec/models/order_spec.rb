29bf3cc88b91793160742456961efc1816648a88
masahito-nagayama <me18sf67m@gmail.com>
2023-02-19 16:24:38 +0900

    specファイルの修正

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe Order, type: :model do
     let(:telephone) { '09011111111' }
     let(:delivery_address) { '東京都杉並区高円寺北2-21-20' }
     let(:payment_method_id) { 1 }
+    let(:other_comment) { "テストコメントです" }
     let(:params) do
       {
         name:,
         email:,
         telephone:,
         delivery_address:,
-        payment_method_id:
+        payment_method_id:,
+        other_comment:
       }
     end
   # ↑ここまではテストデータ valid?メソッドに関しての
@@ -73,6 +75,24 @@ RSpec.describe Order, type: :model do
 
     context '支払い方法が未入力の場合' do
       let(:payment_method_id) {''}
+
+      it { is_expected.to eq false}
+    end
+
+    context 'その他・ご要望が空白の場合' do
+      let(:other_comment) {''}
+
+      it { is_expected.to eq true}
+    end
+
+    context 'その他・ご要望が1000文字の場合' do
+      let(:other_comment) {'あ' * 1_000}
+
+      it { is_expected.to eq true}
+    end
+
+    context 'その他・ご要望が1001文字の場合' do
+      let(:other_comment) {'あ' * 1_001}
 
       it { is_expected.to eq false}
     end
--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "注文フォーム", type: :system do
   let(:email) { 'test@gmail.com' }
   let(:telephone) { '09011111111' }
   let(:delivery_address) { '東京都杉並区高円寺北2-21-20' }
+  let(:other_comment) { 'テストコメントです。' }
 
   it "商品を注文できること" do
     visit new_order_path
@@ -14,6 +15,7 @@ RSpec.describe "注文フォーム", type: :system do
     fill_in "電話番号", with: telephone
     fill_in "お届け先住所", with: delivery_address
     select "銀行振込", from: "支払い方法"
+    fill_in "その他・ご要望", with: other_comment
 
     click_on "確認画面へ"
 
@@ -34,6 +36,7 @@ RSpec.describe "注文フォーム", type: :system do
     expect(order.telephone).to eq telephone
     expect(order.delivery_address).to eq delivery_address
     expect(order.payment_method_id).to eq 2
+    expect(order.other_comment).to eq other_comment
   end
 
   context "入力情報に不備がある場合" do
@@ -45,6 +48,7 @@ RSpec.describe "注文フォーム", type: :system do
       fill_in "電話番号", with: "090111111111"
       fill_in "お届け先住所", with: delivery_address
       select "銀行振込", from: "支払い方法"
+      fill_in "その他・ご要望", with: other_comment
 
       click_on "確認画面へ"
 
@@ -62,6 +66,7 @@ RSpec.describe "注文フォーム", type: :system do
       fill_in "電話番号", with: telephone
       fill_in "お届け先住所", with: delivery_address
       select "銀行振込", from: "支払い方法"
+      fill_in "その他・ご要望", with: other_comment
 
       click_on "確認画面へ"
 
@@ -75,6 +80,7 @@ RSpec.describe "注文フォーム", type: :system do
       expect(page).to have_field "電話番号", with: telephone
       expect(page).to have_field "お届け先住所", with: delivery_address
       expect(page).to have_select "支払い方法", selected: "銀行振込"
+      expect(page).to have_field "その他・ご要望", with: other_comment
 
       click_on "確認画面へ"
 
@@ -95,6 +101,7 @@ RSpec.describe "注文フォーム", type: :system do
       expect(order.telephone).to eq telephone
       expect(order.delivery_address).to eq delivery_address
       expect(order.payment_method_id).to eq 2
+      expect(order.other_comment).to eq other_comment
     end
   end
 end

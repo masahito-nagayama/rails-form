a81c5b0a0abb1919035d8b6bafafc37bc6c396ef
masahito-nagayama <me18sf67m@gmail.com>
2023-02-27 14:45:16 +0900

    system specの修正

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -35,6 +35,7 @@ RSpec.describe "注文フォーム", type: :system do
     visit complete_orders_path
     expect(current_path).to eq new_order_path
 
+    order = Order.last
     expect(order.name).to eq name
     expect(order.email).to eq email
     expect(order.telephone).to eq telephone
@@ -43,9 +44,9 @@ RSpec.describe "注文フォーム", type: :system do
     expect(order.other_comment).to eq other_comment
     expect(order.direct_mail_enabled).to eq true
     expect(order.inflow_source_ids).to eq [1, 4]
-    expect(order.order_prodcuts.size).to eq 1
-    expect(order.order_prodcuts[0].prodcut_id).to eq 3
-    expect(order.order_prodcuts[0].quantity).to eq 3
+    expect(order.order_products.size).to eq 1
+    expect(order.order_products[0].product_id).to eq 3
+    expect(order.order_products[0].quantity).to eq 3
   end
 
   context "入力情報に不備がある場合" do
@@ -134,15 +135,113 @@ RSpec.describe "注文フォーム", type: :system do
       expect(order.other_comment).to eq other_comment
       expect(order.direct_mail_enabled).to eq true
       expect(order.inflow_source_ids).to eq [1, 4]
-      expect(order.order_prodcuts.size).to eq 1
-      expect(order.order_prodcuts[0].prodcut_id).to eq 3
-      expect(order.order_prodcuts[0].quantity).to eq 3
+      expect(order.order_products.size).to eq 1
+      expect(order.order_products[0].product_id).to eq 3
+      expect(order.order_products[0].quantity).to eq 3
     end
   end
 
   context "商品を追加して注文した場合" do
+    it "商品を注文できること" do
+      visit new_order_path
+  
+      fill_in "お名前", with: name
+      fill_in "メールアドレス", with: email
+      fill_in "電話番号", with: telephone
+      fill_in "お届け先住所", with: delivery_address
+      select "銀行振込", from: "支払い方法"
+      select "おいしいたろう(300円/個)", from: "商品"
+      fill_in "数量", with: 3
+
+      click_on "商品を追加する"
+      
+      select "おいしいたろう(300円/個)", from: "order[order_products_attributes][1][product_id]"
+      fill_in "order[order_products_attributes][1][quantity]", with: 3
+      
+      fill_in "その他・ご要望", with: other_comment
+      choose "配信を希望する"
+      check "検索エンジン"
+      check "SNS"
+  
+      click_on "確認画面へ"
+  
+      expect(current_path).to eq confirm_orders_path
+  
+      click_on "OK"
+  
+      expect(current_path).to eq complete_orders_path
+      expect(page).to have_content "#{name}様"
+  
+      # 完了ページを再訪すると、入力画面へ戻る。
+      visit complete_orders_path
+      expect(current_path).to eq new_order_path
+      
+      order = Order.last
+      expect(order.name).to eq name
+      expect(order.email).to eq email
+      expect(order.telephone).to eq telephone
+      expect(order.delivery_address).to eq delivery_address
+      expect(order.payment_method_id).to eq 2
+      expect(order.other_comment).to eq other_comment
+      expect(order.direct_mail_enabled).to eq true
+      expect(order.inflow_source_ids).to eq [1, 4]
+      expect(order.order_products.size).to eq 2
+      expect(order.order_products[0].product_id).to eq 3
+      expect(order.order_products[0].quantity).to eq 3
+      expect(order.order_products[0].product_id).to eq 3
+      expect(order.order_products[0].quantity).to eq 3
+    end
   end
 
   context "商品を追加して、削除してから注文した場合" do
+    it "商品を注文できること" do
+      visit new_order_path
+  
+      fill_in "お名前", with: name
+      fill_in "メールアドレス", with: email
+      fill_in "電話番号", with: telephone
+      fill_in "お届け先住所", with: delivery_address
+      select "銀行振込", from: "支払い方法"
+      select "おいしいたろう(300円/個)", from: "商品"
+      fill_in "数量", with: 3
+
+      click_on "商品を追加する"
+      
+      select "おいしいたろう(300円/個)", from: "order[order_products_attributes][1][product_id]"
+      fill_in "order[order_products_attributes][1][quantity]", with: 3
+
+      click_on "削除", match: :first
+      
+      fill_in "その他・ご要望", with: other_comment
+      choose "配信を希望する"
+      check "検索エンジン"
+      check "SNS"
+  
+      click_on "確認画面へ"
+  
+      expect(current_path).to eq confirm_orders_path
+  
+      click_on "OK"
+  
+      expect(current_path).to eq complete_orders_path
+      expect(page).to have_content "#{name}様"
+  
+      # 完了ページを再訪すると、入力画面へ戻る。
+      visit complete_orders_path
+      expect(current_path).to eq new_order_path
+      
+      order = Order.last
+      expect(order.name).to eq name
+      expect(order.email).to eq email
+      expect(order.telephone).to eq telephone
+      expect(order.delivery_address).to eq delivery_address
+      expect(order.payment_method_id).to eq 2
+      expect(order.other_comment).to eq other_comment
+      expect(order.direct_mail_enabled).to eq true
+      expect(order.inflow_source_ids).to eq [1, 4]
+      expect(order.order_products.size).to eq 1
+      expect(order.order_products[0].product_id).to eq 3
+      expect(order.order_products[0].quantity).to eq 3
+    end
   end
 end
